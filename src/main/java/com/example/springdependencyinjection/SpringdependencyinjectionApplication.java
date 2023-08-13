fe8ef76f8f114da551501565ef5350a2a7cfbb9b
Kaleb Papesh <kaleb.papesh@gmail.com>
2023-08-13 13:44:45 -0700

    Add Spring

---
+++ src/main/java/com/example/springdependencyinjection/SpringdependencyinjectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdependencyinjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdependencyinjectionApplication.class, args);
+	}
+
+}
